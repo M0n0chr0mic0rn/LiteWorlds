2fdbdd579b6922d52c002b9bf8062a635aca7f3e
M0n0chr0mic0rn <115163199+M0n0chr0mic0rn@users.noreply.github.com>
2022-12-25 20:47:27 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,16 +1,16 @@
 <?php
   header('Access-Control-Allow-Origin:*');
 
-  require_once 'maria-user.php'; $user = new User;
-  require_once 'maria-kotia.php'; $kotia = new Kotia;
-  require_once 'maria-litecoin.php'; $ltc = new Litecoin;
-  require_once 'maria-omni.php'; $omni = new Omnilite;
-  require_once 'counter.php'; $counter = new Counter;
+  require_once '/var/www/liteworlds/scripts/maria-user.php'; $user = new User;
+  require_once '/var/www/liteworlds/scripts/maria-kotia.php'; $kotia = new Kotia;
+  require_once '/var/www/liteworlds/scripts/maria-litecoin.php'; $ltc = new Litecoin;
+  require_once '/var/www/liteworlds/scripts/maria-omni.php'; $omni = new Omnilite;
+  require_once '/var/www/liteworlds/scripts/counter.php'; $counter = new Counter;
 
   if (isset($_GET['method']) && sys_getloadavg()[0] < 3.7) {
-    include 'public.php';
-    include 'user.php';
-    include 'core.php';
-    include 'omni.php';
-    include 'kotia.php';
+    include '/var/www/liteworlds/scripts/public.php';
+    include '/var/www/liteworlds/scripts/user.php';
+    include '/var/www/liteworlds/scripts/core.php';
+    include '/var/www/liteworlds/scripts/omni.php';
+    include '/var/www/liteworlds/scripts/kotia.php';
   }
